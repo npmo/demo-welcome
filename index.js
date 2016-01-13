7083a38f75583056577c31813b7671e81a4c6bbb
Benjamin Coe <ben@npmjs.com>
2016-01-13 14:12:09 -0800

    added travis support for demo purposes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var helloString = require('@demo/hello-string')
 var welcomeString = require('@demo/welcome-string')
 
-module.exports = function welcome (name) {
-  return helloString(name) + '! ' + welcomeString();
+function welcome (name) {
+  return helloString(name) + '! ' + welcomeString()
 }
+
+module.exports = welcome
